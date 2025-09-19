789aa7a983699a1225626870aa5fb6691d56a9b9
ritik1510 <kr.ritik1510@gmail.com>
2025-09-19 18:38:54 +0530

    add: the suitable route system for now to manage routing within the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { Layout } from "./components/Layout.jsx";
+import { Switch, Route } from "wouter"
+import RootLayout from "./components/RootLayout.jsx";
+import Projects from "./pages/Projects.jsx";
+import About from "./pages/About.jsx";
+import Skills from "./pages/Skills.jsx";
+import Contact from "./pages/Contact.jsx";
+import Resume from "./pages/Resume.jsx";
+
+function AppRoutes() {
+  return (
+    <RootLayout>
+      <Switch>
+        <Route path='/projects' component={Projects} />
+        <Route path='/skills' component={Skills} />
+        <Route path='/about' component={About} />
+        <Route path='/resume' component={Resume} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+    </RootLayout>
+  );
+}
 
 export default function App() {
   return (
-    <Layout> 
-      <div>
-
-      </div> 
-    </Layout>
+    <>
+      <AppRoutes />
+    </>
   )
 }

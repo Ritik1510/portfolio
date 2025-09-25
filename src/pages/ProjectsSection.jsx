07ad89e74c7b9093ff718b5a6114c248240af9f6
ritik1510 <kr.ritik1510@gmail.com>
2025-09-26 02:18:23 +0530

    add: the whole component to the project tree that helps to render the project card perfectly

--- src/pages/ProjectsSection.jsx
+++ src/pages/ProjectsSection.jsx
@@ -6,11 +6,9 @@ export default function ProjectsSection(){
   // const projects = showAll ? Projects : Projects.slice(0, 3);
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 lg:gap-16 p-8">
       {Projects.map((project) => (
         <ProjectCard key={project.title} project={project} />
       ))}
-    </div>
     </>
   )
 }

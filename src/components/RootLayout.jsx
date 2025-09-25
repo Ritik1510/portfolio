f59c23e3856ad5c9ca2bb0a7a1a29468f1d155d5
ritik1510 <kr.ritik1510@gmail.com>
2025-09-26 02:13:10 +0530

    update the icon for sidebar links

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,24 +1,24 @@
 import { Link } from "wouter";
-import { File } from "lucide-react";
+import verticalBoxMaterial from "../constants/verticalBoxMaterial.jsx";
 
 const sideBarLinks = [
-  { icon: File, label: "Projects", href: "/projects" },
-  { icon: File, label: "Skills", href: "/skills" },
-  { icon: File, label: "About", href: "/about" },
-  { icon: File, label: "Contact", href: "/contact" },
-  { icon: File, label: "Resume", href: "/resume" },
+  { icon: verticalBoxMaterial.fileIcon, label: "Projects", href: "/projects" },
+  { icon: verticalBoxMaterial.fileIcon, label: "Skills", href: "/skills" },
+  { icon: verticalBoxMaterial.fileIcon, label: "About", href: "/about" },
+  { icon: verticalBoxMaterial.fileIcon, label: "Contact", href: "/contact" },
+  { icon: verticalBoxMaterial.fileIcon, label: "Resume", href: "/resume" },
 ];
 
 export default function RootLayout({ children }) {
   return (
-    <div className="main-wrapper h-screen w-screen flex justify-around">
+    <div className="main-wrapper">
       <aside className="left bg-blur w-full h-full flex justify-between flex-col border">
         <div className="hero-heading">
           <h1>Hi I'm Ritik</h1>
         </div>
         <nav className="mt-6">
           {sideBarLinks.map((item) => {
-            const Icon = item.icon;
+            const Icon = item.icon.icon;
             return (
               <Link
                 key={item.href}
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
         </nav>
       </aside>
       {/* Main content */}
-      <main className="center h-full bg-blur">{children}</main>
+      <main className="center h-full w-full bg-blur">{children}</main>
       <aside className="right bg-blur w-full">
         <h2>Right Box Info</h2>
       </aside>

c66240684f006312f66547fac4e323c54e593335
ritik1510 <kr.ritik1510@gmail.com>
2025-12-19 02:11:58 +0530

    adding the tab reference text

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <><div
+      className="tab-tracker-heading mb-2 md:mb-2.5
+      rounded-(--borderRadius-medium) w-full bg-blur card-padding">
+      <p className="">Contact</p>
+    </div></>
   )
 }
 

bbc3029eea767bf3897c4732b3e7709bd9d922b4
ritik1510 <kr.ritik1510@gmail.com>
2025-12-19 16:59:19 +0530

    complete the contact tab

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import { contactData } from "../constants/verticalBoxMaterial"
 
 function Contact() {
   return (
-    <><div
-      className="tab-tracker-heading mb-2 md:mb-2.5
-      rounded-(--borderRadius-medium) w-full bg-blur card-padding">
-      <p className="">Contact</p>
-    </div></>
+    <>
+      <div
+        className="tab-tracker-heading mb-2 md:mb-2.5
+        rounded-(--borderRadius-medium) w-full bg-blur card-padding">
+        <p className="">Contact</p>
+      </div>
+      <div className="contact-wrapper mx-auto w-[70%]">
+        <ul className="contact-ul-wrapper flex flex-col gap-3">
+          {contactData.map((item, index) => (
+            <li key={index} className="contact-li-item flex items-center gap-3 border-b-[0.2367px]">
+              <div className="text-xl">
+                <item.icon />
+              </div>
+              {item.href ? (
+                <a
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="contact-link"
+                >
+                  {item.label}
+                </a>
+              ) : (
+                <span className="contact-text">{item.value}</span>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
 

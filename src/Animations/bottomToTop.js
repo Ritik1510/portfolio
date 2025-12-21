acfa11e669f5e4b1f54ec655349b8fcc7fd1552c
ritik1510 <kr.ritik1510@gmail.com>
2025-12-21 20:38:15 +0530

    bottom to the top animation.

---
+++ src/Animations/bottomToTop.js
@@ -0,0 +1,26 @@
+import gsap from "gsap";
+
+export const bottomToTop = ({
+  targets,
+  y = 80,
+  duration = 0.6,
+  stagger = 0.15,
+  ease = "power3.out",
+}) => {
+  const tl = gsap.timeline({
+    defaults: { duration, ease },
+  });
+
+  tl.fromTo(
+    targets,
+    { y, opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      stagger,
+      clearProps: "transform,opacity",
+    }
+  );
+  console.log("i m animation file")
+  return tl;
+};

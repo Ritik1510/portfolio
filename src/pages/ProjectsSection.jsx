4f066bde67bec57525d210730863fcc937abd7ad
ritik1510 <kr.ritik1510@gmail.com>
2025-12-22 19:23:43 +0530

    manage the card alignment for sm md and lg devices

--- src/pages/ProjectsSection.jsx
+++ src/pages/ProjectsSection.jsx
@@ -11,7 +11,10 @@ export default function ProjectsSection() {
   }, []);
   
   return (<>
-    <div className="center projects-section-wrapper relative">
+    <div className="projects-section-wrapper 
+    relative grid gap-4 grid-cols-1
+    md:grid-cols-[repeat(auto-fit,minmax(280px,1fr))]
+    lg:grid-cols-[repeat(auto-fit,minmax(320px,1fr))]">
       {Projects.map((project) => (
         <ProjectCard key={project.title} project={project} />
       ))}
